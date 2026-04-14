b42bcac1c40af9cc7977d3111db04c39b9368cee
abubakarbaloch496 <abubakarbaloch626@gmail.com>
2026-04-14 10:12:56 -0100

    Add files via upload
    A Python program that demonstrates list operations like append, sort, and reverse.

---
+++ ListOps.py
@@ -0,0 +1,9 @@
+# Demonstrates list operations like append, sort, and reverse
+nums=[9,5,6,7,8,4,3,]
+print(nums)
+nums.append(2)
+print(nums)
+nums.sort()
+print(nums)
+nums.reverse()
+print(nums  )
