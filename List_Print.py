3ece33fde5d36bdcb864986b5f86e14c13ea6a82
abubakarbaloch496 <abubakarbaloch626@gmail.com>
2026-04-14 10:14:49 -0100

    Add files via upload
    A Python program that prints list elements using indexing and a loop.

---
+++ List_Print.py
@@ -0,0 +1,14 @@
+# Prints list elements using indexing and a loop
+names=["Muzamil","Mudasir","Mubashir","Muhammad","Ali","Arif","Rizwan","Farooq"]
+print(names[0])
+print(names[1])
+print(names[2])
+print(names[3])
+print(names[4])
+print(names[5])
+print(names[6])
+print(names[7])
+
+
+for j in names:
+    print(j)
