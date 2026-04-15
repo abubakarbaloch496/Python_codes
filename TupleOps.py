a1c629c08d8176204d8b6000f5fd9f3f727753ac
abubakarbaloch496 <abubakarbaloch626@gmail.com>
2026-04-15 05:36:31 -0100

    Add files via upload
    A Python program that performs built-in operations on a tuple such as count, index, length, max, min, and sum.

---
+++ TupleOps.py
@@ -0,0 +1,8 @@
+# Performs basic tuple operations like count, index, length, max, min, and sum
+data=(1,2,3,4,5,76,3,2,32,23,100,-1)
+print(data.count(23))
+print(data.index(1))
+print(len(data))
+print(max(data))
+print(min(data))
+print(sum(data))
